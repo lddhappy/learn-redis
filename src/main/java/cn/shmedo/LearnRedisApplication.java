88041c05ee32e64c45989b65b3f161b3d9114ae9
liudongdong <liudongdong.2008@outlook.com>
2017-06-05 16:02:00 +0800

    first commit

---
+++ src/main/java/cn/shmedo/LearnRedisApplication.java
@@ -0,0 +1,12 @@
+package cn.shmedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnRedisApplication.class, args);
+	}
+}
